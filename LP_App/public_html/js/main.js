c82f193767035382d44bf7a22b0070d29ff0a2fa
lp-energy <c.rudde@lp-energy.de>
2016-01-15 09:05:46 +0100

    init über svn

--- LP_App/public_html/js/main.js
+++ LP_App/public_html/js/main.js
@@ -1,19 +1,24 @@
 var app = {
-
-    findByName: function() {
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
+    findByName: function () {
         console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
+        this.store.findByName($('.search-key').val(), function (employees) {
             var l = employees.length;
             var e;
             $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
+            for (var i = 0; i < l; i++) {
                 e = employees[i];
                 $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
             }
         });
     },
-
-    initialize: function() {
+    initialize: function () {
         this.store = new MemoryStore();
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
